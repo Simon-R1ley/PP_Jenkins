04882fad2dcb46e722813738a2fddac6a05532bb
Simon Riley <simon.riley@perforce.com>
2023-09-19 11:20:04 +0100

    exec added

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,6 @@
 class jenkins (
   String $jenkinsport = '8080',
 ) {
-# Notification for jenkins linux 
-  notify { 'jenkinslinux':
-  }
   # Installs yum and open jdk 11, ensures the latest - Secure by design as Java 11 is updated
   # but will maintain the security patches
   package {['yum','java-11-openjdk']:
@@ -34,14 +31,6 @@ class jenkins (
     require => [Package['java-11-openjdk'], Yumrepo['jenkins']],
   }
 
-  service { 'jenkins':
-    ensure => 'running',
-    enable => 'true',
-# start     => 'systemctl start /usr/bin/jenkins --httpPort=8000',
-  }
-#require => Service['firewalld'],
-#
-# Resource Type List - Reff: https://www.puppetmodule.info/modules/puppetlabs-stdlib/4.25.1/puppet_types/file_line
   file { '/etc/systemd/system/jenkins.service.d/overide.conf':
     ensure  => file,
     content => epp('jenkins/overide.conf.epp', { 'jenkinsport' => $jenkinsport }),
@@ -49,9 +38,37 @@ class jenkins (
     require => Package['jenkins'], #
   }
 
+  service { 'jenkins':
+    ensure => 'running',
+    enable => 'true',
+  }
+#require => Service['firewalld'],
+#
+# Resource Type List - Reff: https://www.puppetmodule.info/modules/puppetlabs-stdlib/4.25.1/puppet_types/file_line
+
   package { 'firewalld':
     ensure => 'installed',
     before => File['/etc/firewalld/services/jenkins.xml'],
+  }
+
+  file { '/etc/firewalld/services/jenkins.xml':
+    ensure  => file,
+    # source  => 'puppet:///modules/jenkins/jenkins.xml',
+    content => epp('jenkins/jenkins.xml.epp', { 'jenkinsport' => $jenkinsport }),
+    mode    => '0600',
+    owner   => 'root',
+    notify  => Service['firewalld'],
+  }
+
+  exec { 'firewall-cmd --reload':
+    subscribe   => ['/etc/firewalld/services/jenkins.xml'],
+    refreshonly => true,
+  }
+
+  exec { 'firewall-cmd --zone=public --add-service=jenkins --permanent':
+    subscribe   => ['/etc/firewalld/services/jenkins.xml'],
+    refreshonly => true,
+    notify      => Service['firewalld'],
   }
 
   service { 'firewalld':
@@ -68,14 +85,6 @@ class jenkins (
   # 
   # Areas of improvement - 1) use content for file contents - 2) Usage of varibles for port
   #
-  file { '/etc/firewalld/services/jenkins.xml':
-    ensure  => file,
-    # source  => 'puppet:///modules/jenkins/jenkins.xml',
-    content => epp('jenkins/jenkins.xml.epp', { 'jenkinsport' => $jenkinsport }),
-    mode    => '0600',
-    owner   => 'root',
-    notify  => Service['firewalld'],
-  }
 
   # Notify of PW at location  use varible!
 }
