23fabb194dc06633354dced6e7b633fbca7ef0b7
Simon Riley <simon.riley@perforce.com>
2023-09-19 23:13:18 +0100

    Rem jenkins port 8080

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,13 @@
 # @summary A short summary of the purpose of this class
 # https://www.youtube.com/watch?v=1bNscYDeH9s - general infor for roles and profiles!
-# A description of what this class does
+# This module allows for the installation of Jenkins and firewalld configureation which may operate on the same port
 # @param jenkinsport information
 # @example
+# The following structure is adered to: Package, File, Service
 #   include jenkins
+#
 class jenkins (
-  String $jenkinsport = '8080',
+  #String $jenkinsport = '8080',
 ) {
   # Installs yum and open jdk 11, ensures the latest - Secure by design as Java 11 is updated
   # but will maintain the security patches
@@ -23,9 +25,6 @@ class jenkins (
   }
   # This ensures that Jenkins prerequsites are avalible prior to installing the latest version of jenkins 
   # sudo cat /var/lib/jenkins/secrets/initialAdminPassword - possible improvement to show password
-  # Firewall change to allow port 8000
-  # firewall-cmd --zone=public --add-service http
-  # firewall-cmd --zone=public --add-port=8000/tcp
   package { 'jenkins':
     ensure  => latest,
     require => [Package['java-11-openjdk'], Yumrepo['jenkins']],
@@ -35,15 +34,14 @@ class jenkins (
     ensure  => file,
     content => epp('jenkins/overide.conf.epp', { 'jenkinsport' => $jenkinsport }),
     notify  => Service['jenkins'], # Tells jenkins service a change has occured
-    require => Package['jenkins'], #
+    require => Package['jenkins'], # Requieres the package jenkins to be avalible prior to the config being applied
   }
 
   service { 'jenkins':
     ensure => 'running',
     enable => 'true',
   }
-#require => Service['firewalld'],
-#
+# 
 # Resource Type List - Reff: https://www.puppetmodule.info/modules/puppetlabs-stdlib/4.25.1/puppet_types/file_line
 
   package { 'firewalld':
@@ -54,7 +52,6 @@ class jenkins (
 
   file { '/etc/firewalld/services/jenkins.xml':
     ensure  => file,
-    # source  => 'puppet:///modules/jenkins/jenkins.xml',
     content => epp('jenkins/jenkins.xml.epp', { 'jenkinsport' => $jenkinsport }),
     mode    => '0600',
     owner   => 'root',
@@ -77,14 +74,8 @@ class jenkins (
     require => Package['firewalld'],
   }
 
-# File resouce
-  #Firewall port opening for port 8000 - jenkins custom port only 
   # Developing notes - The Puppet source - puppet:/// ... needs to be created - this is done by
   # editing the fileserverconfig = /etc/puppetlabs/puppet/fileserver.conf on the PE master.
   # For more information see: https://www.puppet.com/docs/puppet/6/config_file_fileserver.html
-  # 
-  # Areas of improvement - 1) use content for file contents - 2) Usage of varibles for port
-  #
-
-  # Notify of PW at location  use varible!
+  # echo of PW at location
 }
